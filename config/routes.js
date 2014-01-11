7fd012e72f790976ebcb38f23024826754438225
Stefanie Taepke <s.taepke@student.htw-berlin.de>
2014-01-11 01:34:11 +0100

    search route

--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,9 @@ module.exports = function(app){
 	//home route
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
+	
+	//search route
+	var search = require('../app/controllers/search');
+	app.get('/search', search.index);
 
 };
